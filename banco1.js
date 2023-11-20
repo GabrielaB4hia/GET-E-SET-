dc0b95ff9cf4eb07b3c2ab33e218c44e1e3a5817
GabrielaB4hia <150359606+GabrielaB4hia@users.noreply.github.com>
2023-11-20 11:46:09 -0300

    Add files via upload

---
+++ banco1.js
@@ -0,0 +1,16 @@
+class Pessoa {
+    conta =0 
+    constructor (nome) {
+        this.nome = nome
+    }
+}
+    
+    let p1 = new Pessoa ("Aline")
+    let p2 = new Pessoa ("Junior")
+
+    p1.conta = ("Corrente")
+    p2.conta = ("Poupança")
+
+    console.log (`${p1.nome} tem conta ${p1.conta}`)
+    console.log (`${p2.nome} tem conta ${p2.conta}`)
+    
